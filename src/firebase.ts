905f08a4d8e767afb5bf1af704b2e13218c44345
Shad <shad_1994@hotmail.com>
2024-09-15 21:08:17 +0200

    dot env fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,14 +5,12 @@ import { getAnalytics } from "firebase/analytics"; // Import getAnalytics
 
 // Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyB_EP4-J1lVG9xNUuFxWh6MeL12S6K0SgE",
-  authDomain: "ts-forum-a46e7.firebaseapp.com",
-  databaseURL: "https://ts-forum-a46e7-default-rtdb.firebaseio.com",
-  projectId: "ts-forum-a46e7",
-  storageBucket: "ts-forum-a46e7.appspot.com",
-  messagingSenderId: "315145622202",
-  appId: "1:315145622202:web:38e630277fc80c240e2883",
-  measurementId: "G-GQJH95665Z"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
